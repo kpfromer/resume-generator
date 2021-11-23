9160960c6250029fe75f412aa714d36c9a4bc42d
Kyle Pfromer <kpfromer2@gmail.com>
2021-11-22 21:40:30 -0700

    feat: add working resume generator

--- src/test.tsx
+++ src/test.tsx
@@ -1,7 +1,17 @@
 import * as docxReact from './docx-react';
 
-import { AlignmentType, Document, HeadingLevel, Packer, TabStopPosition, TabStopType } from 'docx';
+import {
+  AlignmentType,
+  Document,
+  HeadingLevel,
+  Packer,
+  TabStopPosition,
+  TabStopType,
+  convertInchesToTwip,
+} from 'docx';
 
+import { DocxMarkdownRenderer } from './markdown';
+import { Resume as ResumeType } from './types';
 import { promises as fs } from 'fs';
 import path from 'path';
 import { yamlToResume } from './yaml';
@@ -43,7 +53,7 @@ const DateText = ({ startDate, endDate, isCurrent }) => {
 
   const text = `${startDateText} - ${endDateText}`;
 
-  return <text>{text}</text>;
+  return <text>{`\t${text}`}</text>;
 };
 
 const removeHttps = (url: string) => url.replace('http://', '').replace('https://', '');
@@ -60,81 +70,153 @@ const insertIntoArray = (arr, value) => {
   }, []);
 };
 
+const margins = {
+  top: 0.01,
+  bottom: 0.01,
+  left: 0.25,
+  right: 0.25,
+};
+
 const Resume = (resume) => (
-  <section>
+  <section
+    page={{
+      margin: {
+        top: convertInchesToTwip(margins.top),
+        bottom: convertInchesToTwip(margins.bottom),
+        left: convertInchesToTwip(margins.left),
+        right: convertInchesToTwip(margins.right),
+        header: 0,
+        footer: 0,
+      },
+    }}
+  >
     <p heading={HeadingLevel.TITLE}>{resume.basics.name}</p>
-
-    <p>{resume.basics.tagLine}</p>
-
-    <p>
+    <p spacing={{ before: convertInchesToTwip(0.05), after: convertInchesToTwip(0.05) }}>
+      {resume.basics.tagLine}
+    </p>
+    <p spacing={{ after: 150 }}>
       {insertIntoArray(
         resume.basics.profiles.map((profile) => (
           <external-link link={profile.url}>
-            <text bold>{removeHttps(profile.url)}</text>
+            <text bold color="#3b82f6">
+              {removeHttps(profile.url)}
+            </text>
           </external-link>
         )),
-        <text>{'     '}</text>,
+        <text>{' | '}</text>,
       )}
     </p>
 
-    <p heading={HeadingLevel.HEADING_1}>Experience</p>
+    <DocxMarkdownRenderer markdown={resume.basics.summary} />
 
-    {resume.work.map((position) => (
-      <p
-        // tabStops={[{ type: TabStopType.RIGHT, position: TabStopPosition.MAX }]}
-        spacing={{ after: 100 }}
-      >
-        <text bold>{position.name}</text>
-        <DateText
-          startDate={position.startDate}
-          endDate={position.endDate}
-          isCurrent={!position.endDate}
-        />
-        <text italics>{position.position}</text>
-        <text>text about position</text>
-      </p>
-    ))}
-
-    <p heading={HeadingLevel.HEADING_1}>Education</p>
-
-    {resume.education.map((school) => (
-      <p
-        // tabStops={[{ type: TabStopType.RIGHT, position: TabStopPosition.MAX }]}
-        spacing={{ after: 100 }}
-      >
-        <text bold>{school.institution}</text>
-        <DateText
-          startDate={school.startDate}
-          endDate={school.endDate}
-          isCurrent={!school.endDate}
-        />
-        <text italics>{school.studyType}</text>
-      </p>
-    ))}
-    {/* <p
-      heading={HeadingLevel.HEADING_1}
-      border={{
-        top: {
-          color: 'auto',
-          space: 1,
-          value: 'single',
-          size: 6,
-        },
-        bottom: {
-          color: 'auto',
-          space: 1,
-          value: 'single',
-          size: 6,
-        },
-      }}
-    >
+    <p heading={HeadingLevel.HEADING_1} thematicBreak spacing={{ before: 100 }}>
       Experience
-    </p> */}
+    </p>
+    {resume.work.map((position, index) => (
+      <>
+        <p
+          tabStops={[{ type: TabStopType.RIGHT, position: convertInchesToTwip(8 - margins.right) }]}
+          spacing={{
+            before: convertInchesToTwip(0.05),
+            after: convertInchesToTwip(0.05),
+          }}
+        >
+          <text bold size={2 * 13}>
+            {position.name}
+          </text>
+          <DateText
+            startDate={position.startDate}
+            endDate={position.endDate}
+            isCurrent={!position.endDate}
+          />
+        </p>
+        <p spacing={{ after: convertInchesToTwip(0.05) }}>
+          <text italics>{position.position}</text>
+        </p>
+        <DocxMarkdownRenderer markdown={position.summary} />
+      </>
+    ))}
+    <p heading={HeadingLevel.HEADING_1} thematicBreak spacing={{ after: 150 }}>
+      Education
+    </p>
+    {resume.education.map((school) => (
+      <>
+        <p
+          tabStops={[{ type: TabStopType.RIGHT, position: convertInchesToTwip(8 - margins.right) }]}
+          spacing={{ before: convertInchesToTwip(0.05), after: convertInchesToTwip(0.05) }}
+        >
+          <text bold size={2 * 13}>
+            {school.institution}
+          </text>
+          <DateText
+            startDate={school.startDate}
+            endDate={school.endDate}
+            isCurrent={!school.endDate}
+          />
+        </p>
+        <p>
+          <text italics>{school.studyType}</text>
+        </p>
+      </>
+    ))}
   </section>
 );
 
 const createResume = (component, config) => {
   const doc = new Document({
+    styles: {
+      default: {
+        title: {
+          run: {
+            font: 'Inter',
+            size: 30 * 2,
+            color: '#E3872D',
+            bold: true,
+          },
+        },
+        heading1: {
+          run: {
+            font: 'Inter',
+            size: 18 * 2,
+            color: '#E3872D',
+            bold: true,
+          },
+        },
+        heading2: {
+          run: {
+            font: 'Inter',
+            size: 14 * 2,
+            color: '#E3872D',
+            bold: true,
+          },
+        },
+        strong: {
+          run: {
+            font: 'Inter',
+          },
+        },
+        hyperlink: {
+          run: {
+            font: 'Inter',
+            color: '#3b82f6',
+          },
+        },
+        listParagraph: {
+          run: {
+            font: 'Inter',
+          },
+        },
+      },
+      paragraphStyles: [
+        // @ts-ignore
+        {
+          name: 'Normal',
+          run: {
+            font: 'Inter',
+          },
+        },
+      ],
+    },
     sections: [component(config)],
   });
 
@@ -150,6 +232,7 @@ const getConfig = async () => {
 
 (async function () {
   const config = await getConfig();
+  // console.dir(toDocx(config.work![1].summary!), null);
   const resumeDocx = createResume(Resume, config);
 
   const buffer = await Packer.toBuffer(resumeDocx);
